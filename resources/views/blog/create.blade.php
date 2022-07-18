317c5a6c0b7d6a1363e524cf6844fe8f8be872b0
Code With Dary <63154066+codewithdary@users.noreply.github.com>
2022-07-18 12:42:45 +0200

    Update create.blade.php

--- resources/views/blog/create.blade.php
+++ resources/views/blog/create.blade.php
@@ -7,37 +7,56 @@
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
-<div class="w-4/5 m-auto text-left">
-    <div class="py-15">
-        <h1 class="text-6xl">
-            Create Post
+<div class="w-4/5 mx-auto">
+    <div class="text-center pt-20">
+        <h1 class="text-3xl text-gray-700">
+            Add new post
         </h1>
+        <hr class="border border-1 border-gray-300 mt-10">
     </div>
-</div>
 
-<div class="w-4/5 m-auto pt-20">
+<div class="m-auto pt-20">
     <form
         action=""
         method=""
         enctype="multipart/form-data">
-        @csrf
+
+        <label for="is_published" class="text-gray-500 text-2xl">
+            Is Published
+        </label>
+        <input
+            type="checkbox"
+            class="bg-transparent block border-b-2 inline text-2xl outline-none"
+            name="is_published">
 
         <input
             type="text"
             name="title"
             placeholder="Title..."
-            class="bg-transparent block border-b-2 w-full h-20 text-6xl outline-none">
+            class="bg-transparent block border-b-2 w-full h-20 text-2xl outline-none">
+
+        <input
+            type="text"
+            name="excerpt"
+            placeholder="Excerpt..."
+            class="bg-transparent block border-b-2 w-full h-20 text-2xl outline-none">
+
+        <input
+            type="number"
+            name="min_to_read"
+            placeholder="Minutes to read..."
+            class="bg-transparent block border-b-2 w-full h-20 text-2xl outline-none">
 
         <textarea
             name="description"
             placeholder="Description..."
             class="py-20 bg-transparent block border-b-2 w-full h-60 text-xl outline-none"></textarea>
 
-        <div class="bg-grey-lighter pt-15">
+        <div class="bg-grey-lighter py-10">
             <label class="w-44 flex flex-col items-center px-2 py-3 bg-white-rounded-lg shadow-lg tracking-wide uppercase border border-blue cursor-pointer">
-                <span class="mt-2 text-base leading-normal">
-                    Select a file
-                </span>
+                    <span class="mt-2 text-base leading-normal">
+                        Select a file
+                    </span>
                 <input
                     type="file"
                     name="image"
